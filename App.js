ed0715656a432147da66d6bd4e75bc5e97096d48
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2023-06-27 00:05:17 +0100

    Working on getting the user's device location information

--- App.js
+++ App.js
@@ -2,15 +2,47 @@
 import { NavigationContainer } from "@react-navigation/native";
 import React, {useState, useEffect} from "react";
 import Tabs from "./src/Components/Tabs";
+import { View, ActivityIndicator, StyleSheet, SafeAreaView, StatusBar } from "react-native";
 
 
 const App = () => {
   const [loading, setloading] = useState(true)
+  const [location, setlocation] = useState(true)
+  const [error, seterror] = useState(true)
+  if (loading) {
+    return(
+      <SafeAreaView style={{marginTop: StatusBar.currentHeight || 0}}>
+        <View style={styles.conainer}>
+          <ActivityIndicator size={'large'} color='blue' />
+        </View>
+      </SafeAreaView>
+    )
+  }
+
+  useEffect(() => {
+    (async()=>{
+      let {status} = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        seterror('Permission to location denied')
+        return
+      }
+      let location = await Location.getCurrentPositionAsync({})
+      setlocation(location)
+    })
+  }, [])
+  
   return (
     <NavigationContainer>
       <Tabs/>
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  conainer: {
+    justifyContent: 'center',
+    flex: 1
+  }
+})
 
 export default App
